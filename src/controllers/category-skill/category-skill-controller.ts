a46b50eae8f5aa43295c9a17bfbe8c93503ccf96
SPF <SPF@SM2HONB17D33SPF>
2024-10-10 17:05:46 +0700

    add: field created_at, and updated_at in some schema

---
+++ src/controllers/category-skill/category-skill-controller.ts
@@ -0,0 +1,7 @@
+import catchErrors from "@helpers/catch-error";
+import { TRequestAuthRoute } from "@types";
+import { Response } from "express";
+
+export const getCategorySkill= catchErrors(async(req:TRequestAuthRoute,res:Response)=>{
+    
+})
